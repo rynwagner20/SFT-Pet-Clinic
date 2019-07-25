f6b29939714940145273ebc3f3b8d58f81597a82
RY <rynwagner20@gmail.com>
2019-07-24 22:34:44 -0400

    Initial Project Add

---
+++ src/main/java/wagner/springframework/sftpetclinic/SftPetClinicApplication.java
@@ -0,0 +1,13 @@
+package wagner.springframework.sftpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SftPetClinicApplication.class, args);
+    }
+
+}
